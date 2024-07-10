32868b71520d92c38ad48af233aeffa61f8c1a91
PriyaWebDesigner <naskartanu@111gmail.com>
2024-07-10 16:18:44 +0530

    PHP LOOP

--- index.php
+++ index.php
@@ -29,6 +29,17 @@ else{
     echo 'Fail <br>';
 }
 
+$result = 20;
+
+if($result>=25){
+    echo 'The Grade is = C';
+}
+
+else{
+    echo 'The Student is Fail in this exam <br>';
+}
+
+
 //Switch Case 
 $color = 'Orange';
 
@@ -50,6 +61,27 @@ switch($color){
    echo 'Failure <br>';
    break;
 }
+
+$color1 = 'Blue';
+
+switch($color1){
+    case 'Green';
+    echo 'Success';
+    break;
+
+    case 'Red';
+    echo 'Danger';
+    break;
+
+    case 'Orange';
+    echo 'Warning';
+    break;
+
+    case 'Blue';
+    echo 'Primary';
+    break;
+}
+echo '<br>';
 
 $marks = 80;
 
@@ -80,32 +112,69 @@ for($i=0; $i<=3; $i++){
     echo $i .'<br>';
 }
 
+$array = [67,46,35];
+
+for($i=0; $i<=2; $i++){
+    echo $array[$i] . ' ';
+}
+echo '<br>';
+
 //for each
 $array = [20,40,50,70];
 
 foreach($array as $element){
     echo $element . ' ';
 }
+echo '<br>';
+
+
+$array = [67,46,35];
+
+foreach($array as $element){
+    echo $element. ' ';
+}
+echo '<br>';
+
 
 //while loop
 $array = [20,40,50,70];
 
 $i = 0;
 while($i<=3){
-    echo $array[$i] . '<br>' . ' ';
+    echo $array[$i] . ' ';
     $i++;
 }
+echo '<br>';
+
+$array = [67,46,35];
+
+$i=0;
+while($i<=2){
+   echo $array[$i] . ' ';
+   $i++;
+}
+echo '<br>';
 
 //do while loop
 $array = [20,40,50,70];
 
-$i=o;
+$i=0;
 do{
     echo $array[$i] . ' ';
     $i++;
 }
 
 while($i<=3);
+echo '<br>';
+
+$array = [67,46,35];
+
+$i=0;
+do{
+    echo $array[$i]. ' ';
+    $i++;
+}
+while($i<=2);
 
 
 
